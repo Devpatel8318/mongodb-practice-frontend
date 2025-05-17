ed0b67cb0797ce7ee4ce6ed64f02a1dc84ef1405
Dev Patel <devpatel@socialpilot.co>
2025-05-17 13:34:05 +0530

    add amplify.yml and webstorm .idea folder

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export default defineConfig({
 
 
 // TODO: add "yarn type-check" in pre-commit hook
-//TODO: add key binding for monaco editor submit
+// TODO: add key binding for monaco editor submit
 // TODO: socket is called multiple times, need to check
 
 */
@@ -28,12 +28,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Toaster, useEffect, useState } from 'src/deps'
 import Routers from 'src/router'
 
-import {
-	evaluateAnswerDispatcher,
-	runOnlyRetrieveDataActionDispatcher,
-} from './features/queryPractice/panels/submissionPanel/submission.actions'
-import { socket } from './socket/socket'
-import { setSocketIdDispatcher } from './socket/socket.action'
+import { initializeSocket } from './socket/socket'
 import { loginUserDispatcher } from './Store/reducers/auth.reducer'
 
 const App = () => {
@@ -45,38 +40,7 @@ const App = () => {
 		const isLoggedIn = localStorage.getItem('isUserLoggedIn') === 'true'
 		if (isLoggedIn) {
 			loginUserDispatcher()
-			socket.on('connect', () => {
-				// default socketId is already set in reducer
-				if (socket.id) {
-					setSocketIdDispatcher(socket.id)
-				}
-				console.log('connected to socket', 'Socket ID:', socket.id)
-			})
-
-			socket.on('pickup', async (data) => {
-				console.log('pickupData', data)
-				const {
-					questionId,
-					question,
-					answer,
-					submissionId,
-					isRunOnly,
-				} = data
-
-				if (isRunOnly) {
-					runOnlyRetrieveDataActionDispatcher({
-						questionId,
-						answer,
-					})
-				} else {
-					evaluateAnswerDispatcher({
-						questionId,
-						question,
-						answer,
-						submissionId,
-					})
-				}
-			})
+			initializeSocket()
 		}
 		setInitialized(true)
 	}, [])
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,15 +1,58 @@
-import { io } from 'socket.io-client'
+import { io, Socket } from 'socket.io-client'
+
+import {
+	evaluateAnswerDispatcher,
+	runOnlyRetrieveDataActionDispatcher,
+} from '../features/queryPractice/panels/submissionPanel/submission.actions'
+import { setSocketIdDispatcher } from './socket.action'
 
 const URL = process.env.REACT_APP_SOCKET_URL
 
-console.log('Socket.IO connecting to:', URL)
+let socket: Socket | null = null
 
-export const socket = io(URL, {
-	reconnectionAttempts: 2,
-	reconnectionDelay: 3000,
-	auth: {
-		token: 'your-auth-token',
-		userId: '12345',
-		userEmail: 'dev@example.com',
-	},
-})
+export const initializeSocket = () => {
+	if (socket) return socket // Return existing socket if already initialized
+
+	console.log('Socket.IO connecting to:', URL)
+
+	socket = io(URL, {
+		reconnectionAttempts: 2,
+		reconnectionDelay: 3000,
+		auth: {
+			token: 'your-auth-token',
+			userId: '12345',
+			userEmail: 'dev@example.com',
+		},
+	})
+
+	// Set up event listeners
+	socket.on('connect', () => {
+		if (socket?.id) {
+			setSocketIdDispatcher(socket.id)
+		}
+		console.log('connected to socket', 'Socket ID:', socket?.id)
+	})
+
+	socket.on('pickup', async (data) => {
+		console.log('pickupData', data)
+		const { questionId, question, answer, submissionId, isRunOnly } = data
+
+		if (isRunOnly) {
+			runOnlyRetrieveDataActionDispatcher({
+				questionId,
+				answer,
+			})
+		} else {
+			evaluateAnswerDispatcher({
+				questionId,
+				question,
+				answer,
+				submissionId,
+			})
+		}
+	})
+
+	return socket
+}
+
+export const getSocket = () => socket
